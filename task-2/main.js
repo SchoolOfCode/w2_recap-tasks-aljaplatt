e1a8e4cc0d6d81882353b0a17241618483b0c8cd
Alex Platt <alja.platt@gmail.com>
2021-11-28 14:39:04 +0000

    task 3 complete

--- task-2/main.js
+++ task-2/main.js
@@ -13,21 +13,16 @@
 
 // task 2b
 
-let p = document.querySelector("p");
+// let p = document.querySelector("p");
 
-let seconds = 0;
+// let seconds = 0;
 
-function timer() {
-  seconds++;
-  p.innerText = seconds;
-  if (seconds === 12) {
-    clearInterval(increaseSeconds);
-  }
-}
-
-let increaseSeconds = setInterval(timer, 1000);
-
-// function myStopFunction() {
-
-//   clearInterval(increaseSeconds);
+// function timer() {
+//   seconds++;
+//   p.innerText = seconds;
+//   if (seconds === 12) {
+//     clearInterval(increaseSeconds);
+//   }
 // }
+
+// let increaseSeconds = setInterval(timer, 1000);
