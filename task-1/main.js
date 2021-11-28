9141c3238ade7fb6b530fe4ad0bf6a8b907dca7c
Alex Platt <alja.platt@gmail.com>
2021-11-28 13:30:10 +0000

    task 2 complete

--- task-1/main.js
+++ task-1/main.js
@@ -8,5 +8,33 @@ const celebs = [
   "Samuel L Jackson",
   "Angelina Jolie",
   "Richard Osman",
-  "Emma Thompson"
+  "Emma Thompson",
 ];
+
+// task 1a
+function makeLegend(name) {
+  return name + " is a now a legend";
+}
+
+console.log(makeLegend("Alex"));
+
+//1b
+
+let legendaryCelebs = celebs.map(makeLegend);
+
+// console.log(legendaryCelebs);
+
+//1c
+
+function startsWithVowel(celebs) {
+  return (
+    celebs.startsWith("A") ||
+    celebs.startsWith("E") ||
+    celebs.startsWith("I") ||
+    celebs.startsWith("O") ||
+    celebs.startsWith("U")
+  );
+}
+let vowelCelebs = celebs.filter(startsWithVowel);
+
+console.log(vowelCelebs);
