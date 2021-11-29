bcd9f7bb7465ad53321099b6ee8f332ee68a789e
Alex Platt <alja.platt@gmail.com>
2021-11-29 12:11:38 +0000

    add list item not working

--- task-4/main.js
+++ task-4/main.js
@@ -26,7 +26,8 @@ let importantThings = [];
 let button = document.getElementById("add-to-list");
 let input = document.getElementById("important-thing");
 // // selected ol from html
-// let addToList = document.querySelector("ol");
+let li = document.createElement("li");
+let list = document.querySelector("ol");
 
 // create callback
 // i want the function to take the value of the input and push to importantThings.
@@ -35,20 +36,18 @@ function getUserInput() {
   importantThings.push(input.value);
   input.value = "";
   console.log(importantThings);
+  addUserInputToList();
 }
 //select input
 // convert array items to list items. map??
 // store in new variable
 // add list items variable  to ol
 function addUserInputToList() {
-  let li = importantThings.map(function (listItem) {
-    listItem += `<li>${listItem}</li>`;
-    document.querySelector("ol").innerHTML = li;
-  });
+  li.innerText = input.value;
+  document.ol.appendChild("li");
 }
 
 // call function
-addUserInputToList();
 
 // add event listener & callback
 button.addEventListener("click", getUserInput);
