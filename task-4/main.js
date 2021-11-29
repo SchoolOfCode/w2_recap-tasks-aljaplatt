205f420d6106bd200a86cf2b435cf8a273aab4bd
Alex Platt <alja.platt@gmail.com>
2021-11-29 09:02:54 +0000

    updated task 4 - incomplete

--- task-4/main.js
+++ task-4/main.js
@@ -1,1 +1,19 @@
+let importantThings = [];
 
+// select button and input
+let button = document.getElementById("add-to-list");
+let input = document.getElementById("important-thing");
+
+// create callback
+// i want the function to take the value of the input and push to importantThings.
+// Then clear
+function getUserInput() {
+  importantThings.push(input.value);
+  input.value = "";
+}
+
+// add event listener & callback
+button.addEventListener("click", getUserInput);
+
+// console.log(inputValue);
+console.log(importantThings);
